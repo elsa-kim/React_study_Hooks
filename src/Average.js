43e8cae72f60cf9a02b820b14f74031f1c86aa34
elsa-kim <ksh96611@naver.com>
2023-04-09 12:23:04 +0900

    hooks_final

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useCallback, useMemo, useRef, useState } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중...");
@@ -10,21 +10,25 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
-  const onChange = (e) => {
-    setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
+  const onChange =
+    ((e) => {
+      setNumber(e.target.value);
+    },
+    []);
+  const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber("");
-  };
+    inputEl.current.focus();
+  }, [number, list]);
 
   const avg = useMemo(() => getAverage(list), [list]);
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
